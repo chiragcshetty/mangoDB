e77000edcf899d0e35abbeabb3e2338cbafa6a5a
Chirag <cshetty2@illinois.edu>
2020-08-05 03:28:08 -0500

    Some scripts not related to app

--- web/txGen.py
+++ web/txGen.py
@@ -7,7 +7,7 @@ f = open("tx.txt", "a")
 
 nPr  = 22;
 prID = range(1,nPr+1);
-cuID = range(4,6);
+cuID = range(2,4);
 
 meatCu  = 0.2*np.ones(nPr+1);meatCu[[4,10,11,13]] = 0.6;meatCu[[12,14]]=0.4;
 veganCu = 0.2*np.ones(nPr+1);veganCu[[15,20]] = 0.6;veganCu[[12,13,14]] = 0.05;
@@ -15,19 +15,21 @@ junkCu  = 0.2*np.ones(nPr+1);junkCu[[19,18]] = 0.5; junkCu[[22,21]]= 0.6;
 
 
 for cu in cuID:
-	if cu<=2:
-		P = meatCu;
-	elif cu<=3:
-		P = veganCu;
-	else:
-		P = junkCu;
+	P = np.zeros(nPr+1); P[[1,2,3,4,5]]=0.7; P[[6,7,8,9,10,11,12,13]]=0.4;P[[14,15,16,17,18,19,20,21]]=0.2;
+	prCnt = np.ones(nPr+1); prAvg = np.zeros(nPr+1); prAvg[[2,4,6,8,10,12]] = 0.5; prAvg[[1,3,5,7,9,11]] = 1; prAvg[range(13,nPr+1)] = 2;
 
-	for txID in range(80,120):
-		dateCu = datetime.date.today()-datetime.timedelta(days = 10*txID)
+	print(prAvg)
+
+	dateCu = datetime.date.today()
+	for txID in range(1000,1100):
+		txID = 1100-txID;
+		dateCu = dateCu-datetime.timedelta(days = 7 + round(1*np.random.randn()))
 		for pr in prID:
 			if np.random.uniform()<P[pr]:
-				f.write(str(txID+1)+','+str(cu)+','+'history,'+str(pr)+','+str(int(3*np.random.uniform())+1)+','+str(dateCu)+',user \n')
-				#f.write(str(pr))
+				f.write(str(txID+1)+','+str(cu)+','+'history,'+str(pr)+','+str(int(prCnt[pr] + np.random.uniform()))+','+str(dateCu)+',user \n')
+				prCnt[pr] = 1;
+			else:
+				prCnt[pr] += prAvg[pr];
 
 
 
